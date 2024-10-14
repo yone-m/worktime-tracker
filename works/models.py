49e560a58c66561c7fac5ea1b93f377154e50857
yone-m <masarres109foot@gmail.com>
2024-10-14 14:05:44 +0900

    workモデルにUserを追加

--- works/models.py
+++ works/models.py
@@ -1,13 +1,13 @@
-from django.db import models
-
 # Create your models here.
 from django.db import models
+from django.contrib.auth.models import User
 
 class Work(models.Model):
     CATEGORY_CHOICES = (
         ("1","365Alert"),
         ("2,","WISE Audit")
     )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     work_name = models.CharField(max_length=200)
     category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
     start_datetime = models.DateTimeField()
